453832f8d00ed8a61214ebf3d2da4e8129d1e749
Kushal B K <Kushal.Bk@Student.HTw-Berlin.de>
2025-10-19 17:37:54 +0200

    M1: Add Movie Entity, Movie Controller

---
+++ src/main/java/de/webtech/project/movie/Movie.java
@@ -0,0 +1,29 @@
+package de.webtech.project.movie;
+
+public class Movie {private String title;
+    private int releaseYear;
+
+
+    //Konstruktor
+    public Movie(String title, int releaseYear){
+        this.title = title;
+        this.releaseYear = releaseYear;
+    }
+    // getter und Setter
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+}
