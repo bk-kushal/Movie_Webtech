6f165cfe5cf0d9c9502f77cae95830db3df64579
Kushal B K <Kushal.Bk@Student.HTw-Berlin.de>
2025-11-22 22:12:00 +0100

    FE Link

--- src/main/java/de/webtech/project/movie/MovieController.java
+++ src/main/java/de/webtech/project/movie/MovieController.java
@@ -1,5 +1,6 @@
 package de.webtech.project.movie;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "https://frontend-movieapp.onrender.com")
 public class MovieController {
 
     @GetMapping("/movies")
